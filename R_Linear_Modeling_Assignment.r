bf52f82b3b0749d71538a4cc145b0063375df14f
gngianni <giarrano.1@buckeyemail.osu.edu>
2023-07-06 17:20:50 +0000

    Added files necessary for assignment

---
+++ R_Linear_Modeling_Assignment.r
@@ -0,0 +1,23 @@
+filename = 'regrex1.csv'
+
+dataset = read.csv(filename)
+
+dataset
+
+model = lm(formula = y ~ x,
+               data = dataset)
+
+library(ggplot2)
+ggplot() +
+  geom_point(aes(x = dataset$x, y = dataset$y),
+             colour = 'dark red')
+
+library(ggplot2)
+ggplot() +
+  geom_point(aes(x = dataset$x, y = dataset$y),
+             colour = 'dark red') +
+  geom_line(aes(x = dataset$x, y = predict(model, newdata = dataset)),
+            colour = 'blue') +
+  ggtitle('Regrex1 Linear Regression') +
+  xlab('x') +
+  ylab('y')
