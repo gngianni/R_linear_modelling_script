3acd57d6efb834f715db82ba108630011ef4e135
gngianni <giarrano.1@buckeyemail.osu.edu>
2023-07-07 20:58:40 +0000

    working on script and testing png files

--- R_script.r
+++ R_script.r
@@ -1,24 +1,35 @@
+#!/usr/bin/env Rscript 
 
-if(length(commandArgs()) != 2) { 
-    stop("Usage: Rscript script_name.R filename.csv")
+args = commandArgs(trailingOnly = TRUE)
+
+if(length(args) == 0) { 
+    stop("Missing filename", call.=FALSE)
+} else if (length(args) == 1) {
+    args[2] = "out.tt"
 }
    
-filename <- commandArgs(trailingOnly = TRUE)[1]
+filename <- args [1]
 
-dataset = read.csv(filename)
+dataset <- read.csv(filename)
 
+message <- sprintf("Loading dataset from %s", filename)
+print(message)
 dataset
 
 model = lm(formula = y ~ x,
                data = dataset)
 
+invisible(png("scatter.png"))
 library(ggplot2)
 ggplot() +
   geom_point(aes(x = dataset$x, y = dataset$y),
-             colour = 'dark red')
-png("file.png")
+             colour = 'dark red') +
+  ggtitle(paste("Scatter plot from", filename)) +
+  xlab('x') +
+  ylab('y')
 dev.off()
 
+png("file.png")
 library(ggplot2)
 ggplot() +
   geom_point(aes(x = dataset$x, y = dataset$y),
@@ -28,5 +39,4 @@ ggplot() +
   ggtitle('Regrex1 Linear Regression') +
   xlab('x') +
   ylab('y')
-png("file.png")
 dev.off()
